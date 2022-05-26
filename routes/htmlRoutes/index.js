58e4bd4acf0808bc9a402b8ad525e4c115cf22d5
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-05-26 11:11:10 -0500

    added comments and readme

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const path = require('path');
 
-// get req route to html homepage
+// get route to html homepage
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
+// notes page
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
